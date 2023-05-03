5b798f700a90666f62d9d96b16121c61507e832f
SantiagoParraGuerrero <santiagoparraguerrero@gmail.com>
2023-05-02 21:35:22 -0500

    data table component fix and rename stuff

---
+++ force-app/main/default/lwc/datatableSample/datatableSample.js
@@ -0,0 +1,33 @@
+import { LightningElement } from 'lwc';
+import { showToastError } from 'c/commonFunctionsHelper';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class CustomLookupSample extends LightningElement {
+    limitOfRecords = 10;
+
+    fields = [
+        { label: 'Name', fieldName: 'Name', primary: true },
+        { label: 'Phone', fieldName: 'Phone', searchable: true },
+        { label: 'AccountNumber', fieldName: 'AccountNumber', searchable: true },
+        { label: 'Owner', fieldName: 'Owner.Name', searchable: true }
+    ];
+
+    get displayFields() {
+        return JSON.stringify(this.fields, null, 2)
+    }
+
+    handleUpdateParameters() {
+        try {
+            const newFieldsValue = this.getElementByDataId('fields');
+            this.fields =JSON.parse(newFieldsValue.value), null, 2;
+        } catch (error) {
+            showToastError.call(this, 'JSON is invalid');
+        }
+
+        this.limitOfRecords = this.getElementByDataId('limitOfRecords').value;
+    }
+
+    getElementByDataId(dataId) {
+        return this.template.querySelector(`[data-id="${dataId}"]`);
+    }
+}
