4bcc432cab8952bc4bed9a8469082c8cbaeb2028
SantiagoParraDev <santiagoparradev@gmail.com>
2025-01-13 22:24:15 -0500

    input filters components

--- force-app/main/default/lwc/componentReferenceList/componentReferenceList.js
+++ force-app/main/default/lwc/componentReferenceList/componentReferenceList.js
@@ -1,8 +1,8 @@
-import { COMPONENTS, MIXINCOMPONENTS, COMPONENT_TYPES } from "c/componentReference";
-import { LightningElement } from "lwc";
+import { COMPONENTS, COMPONENT_TYPES } from "c/componentReference";
+import { LightningElement, track } from "lwc";
 import { MessageChannelMixin } from "c/messageChannelMixin";
 import componentReference from "@salesforce/messageChannel/ComponentReference__c";
-const sections = [
+const sections = Object.freeze([
   {
     label: "Based On SLDS",
     items: Object.values(COMPONENTS)
@@ -20,16 +20,35 @@ const sections = [
         label: component.label,
         name: component.descriptor
       }))
-  },
-];
+  }
+]);
 
 export default class ComponentReferenceList extends MessageChannelMixin(LightningElement) {
-  navigationData = sections;
+  @track navigationData = sections;
   initiallySelected = sections[0].items[0].name;
 
   handleSelect(event) {
     this[MessageChannelMixin.Publish](componentReference, {
       descriptor: event.detail.name
     });
+  }
+
+  handleInputChange(event) {
+    const value = event.detail.value;
+
+    if (value?.length > 2) {
+      this.navigationData = structuredClone(sections).filter((section) => {
+        const filteredComponents = section.items.filter((component) => component.label.includes(event.detail.value));
+
+        if (filteredComponents.length) {
+          section.items = filteredComponents;
+          return true;
+        }
+
+        return false;
+      });
+    } else {
+      this.navigationData = sections;
+    }
   }
 }
