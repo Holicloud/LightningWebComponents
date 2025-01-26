c1844f1dadaca91dd52a1985552c0447f0acd22e
SantiagoParraDev <santiagoparradev@gmail.com>
2025-01-25 23:41:39 -0500

    added component to create examples easier

---
+++ force-app/main/default/lwc/componentExample/componentExample.js
@@ -0,0 +1,16 @@
+import { LightningElement, api } from 'lwc';
+import { classSet } from "c/utils";
+
+const INVERSE_VARIANT = 'inverse';
+
+export default class ComponentExample extends LightningElement {
+  @api title = 'Example title';
+  @api variant;
+
+  get bodyClasses() {
+    return classSet("slds-p-around_medium")
+      .add({ "lgc-bg": this.variant !== INVERSE_VARIANT })
+      .add({ "lgc-bg-inverse": this.variant === INVERSE_VARIANT })
+      .toString();
+  }
+}
